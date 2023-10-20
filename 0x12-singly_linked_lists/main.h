2a6c13037590b17178eff5603c8f5bd19717a2d0
avonggeorge <georgeavong@yahoo.com>
2023-10-19 18:42:24 -0700

    add main.h

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,13 +16,9 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/**
- * main.h -  containing all defined prototype
- */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
