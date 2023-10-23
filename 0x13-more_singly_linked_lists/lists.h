dc5da3c76d912351c6166d1f1ea697add8a441b8
avonggeorge <georgeavong@yahoo.com>
2023-10-23 05:12:09 -0700

    removed trailing whitespace

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,19 +4,17 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * lists.h - this is the header file containg all the prototype in this task.
- *
  * struct listint_s - singly linked list
- * @n: integer (data of the node)
- * @next: pointer to the next node
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
